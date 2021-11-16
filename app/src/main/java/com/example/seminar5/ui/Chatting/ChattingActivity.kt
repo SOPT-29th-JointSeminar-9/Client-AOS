d59273e67d15df9251950417e41b2fe4ab915c7b
권용민 <briandr@naver.com>
2021-11-16 18:56:06 +0900

    [STYLE] chatting shape

---
+++ app/src/main/java/com/example/seminar5/ui/Chatting/ChattingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.seminar5.ui.Chatting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.seminar5.R
+
+class ChattingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatting)
+    }
+}
