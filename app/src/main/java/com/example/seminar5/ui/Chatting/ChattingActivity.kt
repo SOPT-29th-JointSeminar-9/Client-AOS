465a88b724240ec8d36fe1b4767576e95876d6aa
권용민 <briandr@naver.com>
2021-11-19 19:58:42 +0900

    [FEAT]chatting complete

--- app/src/main/java/com/example/seminar5/ui/Chatting/ChattingActivity.kt
+++ app/src/main/java/com/example/seminar5/ui/Chatting/ChattingActivity.kt
@@ -2,15 +2,15 @@ package com.example.seminar5.ui.Chatting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.seminar5.databinding.ActivityChattingBinding
 
 class ChattingActivity : AppCompatActivity() {
-    private lateinit var chattingAdapter: ChatAdapter
+    private lateinit var mynewAdapter:MyNewAdapter
+//    private lateinit var manager : LinearLayoutManager
     private lateinit var binding:ActivityChattingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +18,24 @@ class ChattingActivity : AppCompatActivity() {
 
         binding= ActivityChattingBinding.inflate(layoutInflater)
 
+        binding.clFloatingButton.bringToFront()
+
         val input_edit = binding.etInput
         val send_tv = binding.tvSend
+        val recyclerView = binding.rvChat
+        recyclerView.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
+            Log.v("test", "please1")
 
+            Log.v("test", "please2")
+            recyclerView.scrollToPosition(
+                recyclerView.getAdapter()!!.getItemCount() - 1
+            )
+
+        }
+//        manager = LinearLayoutManager(this)
+
+
+        initAdapter()
 //        input_edit.onFocusChangeListener=View.OnFocusChangeListener{view, b->
 //            if(b)
 //                send_tv.setVisibility(View.VISIBLE)
@@ -36,11 +51,29 @@ class ChattingActivity : AppCompatActivity() {
             else
                 send_tv.visibility=View.GONE
         }
+        send_tv.setOnClickListener{
+            mynewAdapter.chatList.add(ChatData(binding.etInput.text.toString()))
+            mynewAdapter.notifyDataSetChanged()
+            input_edit.text=null
+        }
 
         setContentView(binding.root)
     }
 
     private fun initAdapter(){
+        mynewAdapter = MyNewAdapter()
+
+//        manager.stackFromEnd=true
+//        binding.rvChat.layoutManager=manager
+//        binding.rvChat.setAdapter(mynewAdapter)
+
+        binding.rvChat.adapter=mynewAdapter
+
+//        manager.reverseLayout=true
+
+
+
+
     }
 
 
